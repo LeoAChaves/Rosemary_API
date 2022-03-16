30b63ea2260f61dba072a17096e4ecc702069d27
LeoAChaves <leochaves2121@gmail.com>
2022-03-16 18:04:07 -0300

    Criando banco de dados com SQLite

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import database from "./database/sqlite-db.js";
 
 const app = express();
 app.use(express.json());
---
+++ src/database/create-and-populate.js
@@ -0,0 +1,39 @@
+import sqlite3 from "sqlite3";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+sqlite3.verbose();
+
+const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db";
+const db = new sqlite3.Database(filePath);
+
+const AGENDA_SCHEMA = `CREATE TABLE IF NOT EXISTS "AGENDA" (
+    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Cliente_ID" integer,
+    "Funcionario_ID" integer,
+    "Data" text,
+    "Servico" text,
+    "Duracao" integer
+)`;
+
+const ADD_AGENDA_DATA = `INSERT INTO AGENDA (ID, Cliente_ID, Funcionario_ID, Data, Servico, Duracao)
+VALUES (1, 1, 3, "16/03/2022", "Portifolio_ID 5", 60),
+(2, 2, 1, "17/04/2022", "Portifolio_ID 2", 120),
+(3, 3, 4, "18/05/2022", "Portifolio_ID 15", 30),
+(4, 4, 2, "19/06/2022", "Portifolio_ID 7", 180)`;
+
+function criarTabelaAgenda() {
+  db.run(AGENDA_SCHEMA, (error) => {
+    if (error) console.log("Erro ao criar tabela AGENDA\n" + error.message);
+  });
+}
+
+function popularTabelaAgenda() {
+  db.run(ADD_AGENDA_DATA, (error) => {
+    if (error) console.log("Erro ao popular tabela AGENDA\n" + error.message);
+  });
+}
+
+db.serialize(() => {
+  criarTabelaAgenda();
+  popularTabelaAgenda();
+});
