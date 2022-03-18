105f07c0f8947abea574ec1133576fbaa4581df8
LeoAChaves <leochaves2121@gmail.com>
2022-03-18 20:56:37 -0300

    Usando CORS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import database from "./database/sqlite-db.js";
 import AgendaController from "./controllers/agendaController.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 AgendaController(app, database);
 
