96d9e9efa142c875f2aa6bfa9728cc3b35959087
LeoAChaves <leochaves2121@gmail.com>
2022-03-18 13:43:26 -0300

    Refatorando código "agendaController.js"

--- src/controllers/agendaController.js
+++ src/controllers/agendaController.js
@@ -3,15 +3,21 @@ import AgendaDAO from "../DAO/agendaDAO.js";
 const AgendaController = (app, bd) => {
   const agendaDAO = new AgendaDAO(bd);
 
-  app.get("/agenda", (req, res) => {
-    agendaDAO
-      .pegarTodosAgendamentos()
-      .then((resposta) => {
-        res.json(resposta);
-      })
-      .catch((error) => {
-        res.json(error);
-      });
+  app.get("/agenda", async (req, res) => {
+    try {
+      const pegarTodosAgendamentos = await agendaDAO.pegarTodosAgendamentos();
+      res.json(pegarTodosAgendamentos);
+    } catch (error) {
+      res.json(error);
+    }
+    // agendaDAO
+    //   .pegarTodosAgendamentos()
+    //   .then((resposta) => {
+    //     res.json(resposta);
+    //   })
+    //   .catch((error) => {
+    //     res.json(error);
+    //   });
   });
 };
 
