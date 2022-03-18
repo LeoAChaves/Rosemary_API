02c2e130db33e4690765a8644aa28fc085f84af6
LeoAChaves <leochaves2121@gmail.com>
2022-03-18 14:21:43 -0300

    Adicionando "hora" e formatanto "data" (DB)

--- src/database/create-and-populate.js
+++ src/database/create-and-populate.js
@@ -11,16 +11,17 @@ const AGENDA_SCHEMA = `CREATE TABLE IF NOT EXISTS "AGENDA" (
     "Cliente_ID" integer,
     "Funcionario_ID" integer,
     "Data" text,
+    "Hora" datetime,
     "Servico" text,
     "Duracao" integer
 )`;
 
-const ADD_AGENDA_DATA = `INSERT INTO AGENDA (Cliente_ID, Funcionario_ID, Data, Servico, Duracao)
-VALUES (1, 3, "16/03/2022", "Portifolio_ID 5", 60),
-(2, 1, "17/04/2022", "Portifolio_ID 2", 120),
-(3, 4, "18/05/2022", "Portifolio_ID 15", 30),
-(4, 2, "19/06/2022", "Portifolio_ID 7", 180),
-(5, 2, "19/06/2022", "Portifolio_ID 10", 90)`;
+const ADD_AGENDA_DATA = `INSERT INTO AGENDA (Cliente_ID, Funcionario_ID, Data, Hora, Servico, Duracao)
+VALUES (1, 3, "2022-03-09", "09:30:00", "Portifolio_ID 5", 60),
+(2, 1, "2022-04-21", "14:00:00", "Portifolio_ID 2", 120),
+(3, 4, "2022-05-16", "10:45:00", "Portifolio_ID 15", 30),
+(4, 2, "2022-06-12", "16:50:00","Portifolio_ID 7", 180),
+(5, 2, "2022-07-25", "18:15:00","Portifolio_ID 10", 90)`;
 
 function criarTabelaAgenda() {
   db.run(AGENDA_SCHEMA, (error) => {
