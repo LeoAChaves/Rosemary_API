7906ac172d6e69e358150c5d32cf17025495b3fb
LeoAChaves <leochaves2121@gmail.com>
2022-03-21 20:40:31 -0300

    Efetuendo "curto circuito" para a PORT

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor aberto na http://localhost:${port}`);
