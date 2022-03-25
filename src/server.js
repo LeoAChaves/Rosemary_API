8bd0838cae94269471df3c3e52c4ed72a84a8801
LeoAChaves <leochaves2121@gmail.com>
2022-03-25 12:43:09 -0300

    Tentando solucionar problemas com o Heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor aberto na http://localhost:${port}`);
